7eac100dfa0eac194ddd9bbf466b03db4e8aea0c
bha6 <brianha93@gmail.com>
2016-10-15 19:59:40 -0400

    added extra functioanlities to buttons

--- extension/javascript/upDownVote.js
+++ extension/javascript/upDownVote.js
@@ -8,12 +8,11 @@ var testServer = {
   "linkBaitFlag": false,
   "uxRating": 0,
   "contentRating": 0,
-  "generalRating": 1   // 1 is upvote, 2 is downvote, 0 is no vote
+  "generalRating": 0   // 1 is upvote, 2 is downvote, 0 is no vote
 };
-/*
-	increase upvotes
-*/
 
+
+// general votes
 $("#upgeneral").click(function (){
 	var serverData = testServer;
 	serverData.generalRating = 1;
@@ -23,6 +22,34 @@ $("#upgeneral").click(function (){
 $("#downgeneral").click(function (){
 	var serverData = testServer;
 	serverData.generalRating = 2;
+	updateData(serverData);
+});
+
+
+//content votes
+$("#upcontent").click(function (){
+	var serverData = testServer;
+	serverData.contentRating = 1;
+	updateData(serverData);
+});
+
+$("#downcontent").click(function (){
+	var serverData = testServer;
+	serverData.contentRating = 2;
+	updateData(serverData);
+});
+
+
+//experience votes
+$("#upexperience").click(function (){
+	var serverData = testServer;
+	serverData.uxRating = 1;
+	updateData(serverData);
+});
+
+$("#downexperience").click(function (){
+	var serverData = testServer;
+	serverData.uxRating = 2;
 	updateData(serverData);
 });
 
@@ -37,8 +64,13 @@ $("#experience").click(function(){
 
 var getData = function(){
 	$.get( getURL, function( data ) {
-	  $( "#upvotes" ).html( data.up );
-	  $( "#downvotes" ).html( data.down );
+	  $( "#upGeneralVote" ).html( data.up );
+	  $( "#downGeneralVote" ).html( data.down );
+  	  $( "#upContentVote" ).html( data.contentUp );
+	  $( "#downContentVote" ).html( data.contentDown );
+  	  $( "#upExperienceVote" ).html( data.uxUp );
+	  $( "#downExperienceVote" ).html( data.uxDown );
+	  console.log(data);
 	});
 };
 
