6567b4def65a0af6ef0f1c44e2f08706c83dc08d
Long Pham <Long Pham>
2016-10-15 14:19:53 -0400

    popup.js added and jquery

---
+++ extension/javascript/upDownVote.js
@@ -0,0 +1,23 @@
+$("#general").click(function(){
+	alert("you clicked general");
+});
+
+$("#content").click(function(){
+	alert("you clicked content");
+});
+
+$("#experience").click(function(){
+	alert("you clicked experience");
+});
+
+
+
+// // Pure JS:
+// document.addEventListener('DOMContentLoaded', function() {
+//   document.getElementById("general").addEventListener("click", handler);
+// });
+
+// // The handler also must go in a .js file
+// function handler() {
+//   alert("hello!");
+// }
