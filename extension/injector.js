23e7a03946a4841c0c8b3c796a2fd35a43780950
Elvis Sun <elvissunlihe@gmail.com>
2016-10-15 12:39:48 -0400

    Injector.js

--- extension/injector.js
+++ extension/injector.js
@@ -1,5 +1,25 @@
-function filterResultInserts(event) {
-  console.log(event);
+var bkg = chrome.extension.getBackgroundPage();
+bkg.console.log('foo');
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
 }
 
-target.addEventListener('DOMNodeInserted', filterResultInserts);
+window.onload = onWindowLoad;
