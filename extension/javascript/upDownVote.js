ea18adf8c5aab94672f594991976509743b67418
bha6 <brianha93@gmail.com>
2016-10-15 16:33:17 -0400

    added GET and POST functionalities for testing purposes.

--- extension/javascript/upDownVote.js
+++ extension/javascript/upDownVote.js
@@ -1,6 +1,31 @@
-$("#general").click(function(){
-	alert("you clicked general");
+var getURL = "https://the-one.herokuapp.com/api/reviews/score/www.google.com";
+var postURL = "https://the-one.herokuapp.com/api/reviews";
+
+var testServer = {
+  "domain": "www.google.com",
+  "moneyFlag": false,
+  "virusFlag": false,
+  "linkBaitFlag": false,
+  "uxRating": 0,
+  "contentRating": 0,
+  "generalRating": 1   // 1 is upvote, 2 is downvote, 0 is no vote
+};
+/*
+	increase upvotes
+*/
+
+$("#upgeneral").click(function (){
+	var serverData = testServer;
+	serverData.generalRating = 1;
+	updateData(serverData);
 });
+
+$("#downgeneral").click(function (){
+	var serverData = testServer;
+	serverData.generalRating = 2;
+	updateData(serverData);
+});
+
 
 $("#content").click(function(){
 	alert("you clicked content");
@@ -10,14 +35,24 @@ $("#experience").click(function(){
 	alert("you clicked experience");
 });
 
+var getData = function(){
+	$.get( getURL, function( data ) {
+	  $( "#upvotes" ).html( data.up );
+	  $( "#downvotes" ).html( data.down );
+	});
+
+};
+
+var updateData = function(theServer){
+	$.post( postURL, theServer, function(){
+		/* some callback thing here */
+		getData();
+	});
+}
 
 
-// // Pure JS:
-// document.addEventListener('DOMContentLoaded', function() {
-//   document.getElementById("general").addEventListener("click", handler);
-// });
+var init = function(){
+	getData();
+};
 
-// // The handler also must go in a .js file
-// function handler() {
-//   alert("hello!");
-// }
+init();
