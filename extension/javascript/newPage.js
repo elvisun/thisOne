7db439bd4c60c00ee1dec181f09a6eb68168b51b
Elvis Sun <elvissunlihe@gmail.com>
2016-10-15 21:54:58 -0400

    split up real & testing

--- extension/javascript/newPage.js
+++ extension/javascript/newPage.js
@@ -1,29 +1,59 @@
 // Checking page title
-console.log("running");
-if (true) {
-	console.log("in if");
+
     //Creating Elements
-    setTimeout(function(){
-    	var style = document.createElement('style');
-		style.type = 'text/css';
-		style.innerHTML = '.conditionalFormat { color: #F00; }';
-		document.getElementsByTagName('head')[0].appendChild(style);
+var useRealData = true;
 
-    	var t = document.getElementsByClassName("r");
-    	for (var i = 0; i < t.length; i++){
-    		var url = t[i].childNodes[0].href;
-    		var domain = extractDomain(url);
-    		console.log(domain);
+setTimeout(function(){
+    //$('head').append('<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">');
 
-			var textnode = document.createElement("span"); 
-			var q = document.createTextNode("  " + "5.0");       // Create a text node
-			textnode.appendChild(q);
-			textnode.className += "conditionalFormat"
-			t[i].appendChild(textnode);
-		}
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = '.upvote { color: green; font-size: 14px};';
+	document.getElementsByTagName('head')[0].appendChild(style);
 
-    }, 2000);
-}
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.downvote { color: red; font-size: 14px };';
+    document.getElementsByTagName('head')[0].appendChild(style);
+
+	var t = document.getElementsByClassName("r");
+	for (var i = 0; i < t.length; i++){
+		var url = t[i].childNodes[0].href;
+		var domain = extractDomain(url);
+        
+        var up = 0;
+        var down = 0;
+        if (useRealData){
+            $.get(
+                "https://the-one.herokuapp.com/api/reviews/score/" + domain,
+                {paramOne : 1, paramX : 'abc'},
+                function(data) {
+                   up = data.up;
+                   down = data.down;
+                }
+            );
+        }
+        else{
+            up = getVotes()
+            down = getVotes()
+        }
+
+		var upnode = document.createElement("span"); 
+		var q = document.createTextNode("  " + up.toString());       // Create a text node
+        upnode.appendChild(q);
+        upnode.className += "upvote st";
+		t[i].appendChild(upnode);
+
+        var downnode = document.createElement("span"); 
+        var q = document.createTextNode(" " + down.toString());       // Create a text node
+        downnode.appendChild(q);
+        downnode.className += "downvote st";
+        t[i].appendChild(downnode);
+
+	}
+    console.log("done");
+
+}, 1000);
 
 
 function extractDomain(url) {
@@ -42,10 +72,16 @@ function extractDomain(url) {
     return domain;
 }
 
-function httpGet(theUrl)
-{
+function httpGet(theUrl, callback){
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function getVotes(){
+    return Math.round((100 + Math.random()*5000));
 }
