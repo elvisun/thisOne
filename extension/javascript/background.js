1b0e080a51334af4f7ae7db0b292ba8628328082
Long Pham <Long Pham>
2016-10-15 21:46:43 -0400

    added functionality to pull domain name and fill database if none exists, voting works

--- extension/javascript/background.js
+++ extension/javascript/background.js
@@ -15,28 +15,3 @@ chrome.extension.onRequest.addListener(
   });
 
 
-
-// chrome.tabs.getSelected(null, function(tab) {
-
-//   // Now inject a script onto the page
-//   chrome.tabs.executeScript(tab.id, {
-//        code: "chrome.extension.sendRequest({content: document.body.innerHTML}, function(response) { console.log('success'); });"
-//      }, function() { console.log('done'); });
-
-// });
-
-// Now inject a script onto the page
-chrome.tabs.executeScript(tab.id, {
-   code: "chrome.extension.sendRequest({content: document.body.innerHTML}, function(response) { console.log('success'); });"
- }, function() { console.log('done'); });
-
-
-// Checking page title
-if (document.title.indexOf("Google") != -1) {
-    //Creating Elements
-    var btn = document.createElement("BUTTON")
-    var t = document.createTextNode("CLICK ME");
-    btn.appendChild(t);
-    //Appending to DOM 
-    document.body.appendChild(btn);
-}
--- extension/javascript/upDownVote.js
+++ extension/javascript/upDownVote.js
@@ -1,8 +1,10 @@
 var getURL = "https://the-one.herokuapp.com/api/reviews/score/www.google.com";
 var postURL = "https://the-one.herokuapp.com/api/reviews";
+var domainName = "";
+
 
 var testServer = {
-  "domain": "www.google.com",
+  "domain": domainName,
   "moneyFlag": false,
   "virusFlag": false,
   "linkBaitFlag": false,
@@ -15,13 +17,17 @@ var testServer = {
 // general votes
 $("#upgeneral").click(function (){
 	var serverData = testServer;
+	serverData.domain = domainName;
 	serverData.generalRating = 1;
+	resetServerProperties();
 	updateData(serverData);
 });
 
 $("#downgeneral").click(function (){
 	var serverData = testServer;
+	serverData.domain = domainName;
 	serverData.generalRating = 2;
+	resetServerProperties();
 	updateData(serverData);
 });
 
@@ -29,13 +35,17 @@ $("#downgeneral").click(function (){
 //content votes
 $("#upcontent").click(function (){
 	var serverData = testServer;
+	serverData.domain = domainName;
 	serverData.contentRating = 1;
+	resetServerProperties();
 	updateData(serverData);
 });
 
 $("#downcontent").click(function (){
 	var serverData = testServer;
+	serverData.domain = domainName;
 	serverData.contentRating = 2;
+	resetServerProperties();
 	updateData(serverData);
 });
 
@@ -43,26 +53,41 @@ $("#downcontent").click(function (){
 //experience votes
 $("#upexperience").click(function (){
 	var serverData = testServer;
+	serverData.domain = domainName;
 	serverData.uxRating = 1;
+	resetServerProperties();
 	updateData(serverData);
 });
 
 $("#downexperience").click(function (){
 	var serverData = testServer;
+	serverData.domain = domainName;
 	serverData.uxRating = 2;
+	resetServerProperties();
 	updateData(serverData);
 });
 
 
-$("#content").click(function(){
-	alert("you clicked content");
-});
+var getWebsiteURL = function(){
+	console.log("get web URL");
+	chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+	    var url = tabs[0].url;
+	    console.log("the URL is: " + url);
 
-$("#experience").click(function(){
-	alert("you clicked experience");
-});
+	    //parse URL here
+	    // REGEX TO FIND DOMAIN NAME
+	    var domain = url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+	    // console.log(domain);
+	    domainName = domain;
+	    getData();
+
+	});
+}
+
 
 var getData = function(){
+	console.log("get data");
+	getURL = "https://the-one.herokuapp.com/api/reviews/score/"+domainName;
 	$.get( getURL, function( data ) {
 	  $( "#upGeneralVote" ).html( data.up );
 	  $( "#downGeneralVote" ).html( data.down );
@@ -70,20 +95,33 @@ var getData = function(){
 	  $( "#downContentVote" ).html( data.contentDown );
   	  $( "#upExperienceVote" ).html( data.uxUp );
 	  $( "#downExperienceVote" ).html( data.uxDown );
-	  console.log(data);
 	});
 };
 
 var updateData = function(theServer){
+	postURL = "https://the-one.herokuapp.com/api/reviews";
+
 	$.post( postURL, theServer, function(){
 		/* some callback thing here */
 		getData();
 	});
 }
 
+var resetServerProperties = function(){
+	testServer = {
+	  "domain": getWebsiteURL(),
+	  "moneyFlag": false,
+	  "virusFlag": false,
+	  "linkBaitFlag": false,
+	  "uxRating": 0,
+	  "contentRating": 0,
+	  "generalRating": 0   // 1 is upvote, 2 is downvote, 0 is no vote
+	};
+}
 
 var init = function(){
-	getData();
+	getWebsiteURL();
+	// getData();
 };
 
 init();
